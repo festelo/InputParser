45afbc223d517e0c901476430c188695a4f25b99
festelo <mail@festelo.tk>
2018-04-28 00:25:04 +0300

    exampleupdate

--- InputParserExample/Program.cs
+++ InputParserExample/Program.cs
@@ -1,36 +1,41 @@
 ﻿using System;
-using System.Reflection;
 using System.Threading;
 using InputParser;
 
 namespace InputParserExample
 {
+    class TimeSpanParsedParameterAttribute : ParsedParameterAttribute
+    {
+        public override object Parse(string s) => TimeSpan.Parse(s);
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Parser.Init(typeof(InputClass), Console.In, Console.Out);
-            while (true)
-            {
-                Thread.Sleep(10000);
-            }
+            Parser.InitFromThisThread(typeof(InputClass), Console.In, Console.Out);
         }
     }
 
     public static class InputClass
     {
-        [InputFunction("say", "hello", "sayhello")]
+        [DefaultInput]
+        public static string Default()
+        {
+            return "Input is empty";
+        }
+
+        [Input("say", "hello", "sayhello")]
         public static string SayHello()
         {
             return "Hello";
         }
-        [InputFunction]
+        [Input]
         public static int Sum(int a, int b)
         {
             return a + b;
         }
-        [InputFunction]
-        public static TimeSpan AddHours([InputParameter(typeof(TimeSpan), nameof(TimeSpan.Parse))]TimeSpan time, int hourse)
+        [Input]
+        public static TimeSpan AddHours([TimeSpanParsedParameter] TimeSpan time, int hourse)
         {
             return time.Add(new TimeSpan(hourse,0,0));
         }
