3f4936ace65958015cffde77d4b0862ce3167651
festelo <mail@festelo.tk>
2018-04-28 19:48:29 +0300

    Bug fixes

---
+++ InputParser/Attributes/ReadingEndedAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InputParser
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ReadingEndedAttribute : InputAttribute
+    {
+        public ReadingEndedAttribute() : base(names: null) { }
+    }
+}
